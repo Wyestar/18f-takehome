3fde702522e7ab36d568918dbb1f027f8bc3e6b7
Douglas Chin <food.bolus@gmail.com>
2020-07-12 13:45:53 -0400

    edit form wip

--- src/components/posts/PostEdit.js
+++ src/components/posts/PostEdit.js
@@ -7,32 +7,82 @@ import { putPost } from '../../store/actions';
 // enable edit of title that changes post
 class PostEdit extends React.Component {
 	state = {
-		title: "",
-		body: "",
-		userId: 0,
-		id: 0
+		enableEditTitle: false,
+		enableEditBody: false,
+		editedPost: {
+			title: "",
+			body: "",
+			userId: 0,
+			id: 0
+		}
 	};
 
-	onChangeHandler = (event) => {
+	componentDidMount() {
+		this.setState({
+			editedPost: Object.assign({}, this.state.editedPost, this.props.location.post)
+		})
+	}
+
+	onEditTitleChangeHandler = (event) => {
+		// event.preventDefault();
+		console.error('PE edit title submit: ', event.target.value)
+		this.setState({
+			editedPost: {
+				title: event.target.value
+			}
+		})
+
+	};
+
+	onEditTitleSubmitHandler = (event) => {
 		event.preventDefault();
+		console.error('PE edit title submit: ', event.target.value)
+		// send new post data to store
+
 
 	};
 
-	onSubmitHandler = (event) => {
-		// send new post data to store
+	enableEditTitleHandler = (event) => {
+		event.preventDefault();
+		this.setState({
+			enableEditTitle: true
+		})
 	};
 
 	render() {
-		const postData = this.props.location.post
+		const postData = this.props.location.post;
+		// postData.id, postData.userId
 		console.log('PE props: ', postData);
+		console.log('PE state: ', this.state.editedPost);
 		return (
 			<div>
 				post edit2
-				===
-				{postData.title}
-				===
-				{postData.body}
+				<div className="ui segment">
 
+					{postData.title}
+
+					<button className="ui button" onClick={this.enableEditTitleHandler}>enable edit button</button>
+
+					{ this.state.enableEditTitle &&
+
+						<form onSubmit={this.onEditTitleSubmitHandler} className="ui form">
+							<label>edit this post's title</label>
+							<input
+								type="text"
+								value={this.state.editedPost.title}
+								onChange={this.onEditTitleChangeHandler}
+							/>
+							<input className="ui button secondary" type="submit" value="Edit title"/>
+							<input className="ui button primary" type="submit" value="Search exact title"/>
+						</form>
+					}
+					<div>break</div>
+
+					{/*<form onSubmit={this.onEditBodySubmitHandler} className="ui form">*/}
+					{/*	{post.body}*/}
+					{/*	<input className="ui button secondary" type="submit" value="Edit body"/>*/}
+					{/*</form>*/}
+				</div>
 			</div>
 		)
 	}
